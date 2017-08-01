3f11180e571691955abfadcc77c318385ae7ee75
Kevin Moore <kevmoo@users.noreply.github.com>
2017-08-01 08:15:50 -0700

    Rename exit_code file to match class name (#11)

--- lib/io.dart
+++ lib/io.dart
@@ -2,7 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-export 'src/exit_codes.dart' show ExitCode;
+export 'src/exit_code.dart' show ExitCode;
 export 'src/permissions.dart' show isExecutable;
 export 'src/process_manager.dart' show ProcessManager, Spawn;
 export 'src/shared_stdin.dart' show SharedStdIn, sharedStdIn;
